2a5233297b27c4dd9049b2f69e8e5b7fe2651c3d
Sachin-Singh-Baghel <90504570+Sachin-Singh-Baghel@users.noreply.github.com>
2023-01-18 17:38:42 +0530

    push

---
+++ database.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+var con = mysql.createConnection({
+    host: "165.232.151.6",
+    user: "raj",
+    password: "Kamingo@11",
+    database: "Sachin_DB",
+  
+  
+  });
+
+
+
+  module.exports = con
--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const con = require('./database.js')
 
 const port = 8080
 
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/contact', (req, res) => {
+  res.render('contact')
+})
+
+app.post('/contact', (req, res) => {
+  var data = req.body;
+  console.log(data);
+  console.log("You have been registered");
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO DB_Table (name, email , description) VALUES ?";
+    var values = [
+      [req.body.Name,req.body.Gmail,req.body.Des]
+    ];
+    con.query(sql, [values], function (err, result) {
+      if (err) throw err;
+      console.log("Number of records inserted: " + result.affectedRows);
+    });
+  });
+  res.redirect("/")
+})
 
 
 
